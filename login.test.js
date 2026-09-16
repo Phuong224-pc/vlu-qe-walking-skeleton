836133e699e051b6b9278c1eabfd44bfb0f91a1e
Trương Trần Thanh Phuong <phuongtruong121204@gmail.com>
2026-09-16 14:26:41 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,1 +1,9 @@
+const login = require('./auth');
 
+test('Dang nhap thanh cong voi admin/123', () => {
+    expect(login('admin', '123')).toBe(true);
+});
+
+test('Dang nhap me hoac sai mat khau', () => {
+    expect(login('admin', 'wrongpass')).toBe(false);
+});
